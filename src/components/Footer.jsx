6b1a1a50f817fb1767ba482eecfee07f988aaa24
acr-coder <acarerisa1@gmail.com>
2021-11-25 17:44:30 +0300

    responsive not finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
-import { Email, Facebook, Instagram, LinkedIn, Phone, Pinterest, Room, Twitter, YouTube } from '@material-ui/icons';
+import { Email, Facebook, Instagram, Phone, Pinterest, Room, Twitter } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
-
+import { mobile } from '../responsive';
 
 const Container = styled.div`
     display: flex;
+    ${mobile({ flexDirection: "column" })}
 `;
 const Left = styled.div`
     flex: 1;
@@ -35,6 +36,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile({ display: "none" })}
 `;
 const Title = styled.h3`
     margin-bottom: 30px;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,58 +3,70 @@ import styled from 'styled-components'
 import SearchIcon from '@material-ui/icons/Search';
 import { Badge } from '@material-ui/core';
 import { ShoppingCartOutlined } from '@material-ui/icons';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
-    height: 60px;
-    
+  height: 60px;
+  ${mobile({ height: "50px" })}
 `;
+
 const Wrapper = styled.div`
-padding: 10px 20px;
-display: flex;
-justify-content: space-between;
-align-items: center;
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Left = styled.div`
-flex: 1;
-display: flex;
-align-items: center;
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 const Language = styled.span`
-    font-size: 14px;
-    cursor: pointer;
+  font-size: 14px;
+  cursor: pointer;
+  ${mobile({ display: "none" })}
 `;
+
 const SearchContainer = styled.div`
-    border: 1px solid lightcoral;
-    display:flex;
-    align-items: center;
-    margin-left: 25px;
-    padding: 5px;
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
 `;
+
 const Input = styled.input`
-    border: none;
-    outline: none;
+  border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
-flex: 1;
-text-align: center;
+  flex: 1;
+  text-align: center;
 `;
-const Logo = styled.h2`
-    font-weight: bold;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
 `;
+
 const Right = styled.div`
-flex: 1;
-display: flex;
-align-items: center;
-justify-content: flex-end;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
 `;
+
 const MenuItem = styled.div`
-font-size: 14px;
-cursor: pointer;
-margin-left: 25px;
-`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+  ${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`;
 
 const Navbar = () => {
     return (
@@ -63,12 +75,12 @@ const Navbar = () => {
             <Left>
                 <Language>EN</Language>
                 <SearchContainer>
-                    <Input />
+                    <Input placeholder="search..." />
                     <SearchIcon style={{color:"gray", fontSize:16}} />
 
                 </SearchContainer>
             </Left>
-            <Center><Logo>ACR-DEV</Logo></Center>
+            <Center><Logo>ACR. </Logo></Center>
             <Right>
                 <MenuItem>REGISTER</MenuItem>
                 <MenuItem>SIGN IN</MenuItem>
